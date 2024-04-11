1d914f796f943624c774000b3e2a39f3ef3c07d4
YassenMohamed7 <yassenmohammed871@gmail.com>
2024-04-11 11:57:42 +0200

    handling undefined routes

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
 const productRoutes = require('./Routes/productRoutes');
 const categoryRoutes = require('./Routes/categoryRoutes');
+const userRoutes = require('./Routes/userRoutes');
 
 
+dotenv.config({path: './Config/config.env'});
 const app = express();
 app.use(express.json());
 
 
+// Mount Routes
 app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/categories', categoryRoutes);
+app.use('/api/v1/users', userRoutes);
+
+app.all('*', (req, res, next) => {
+    const err = new Error('Page not found');
+    next(err);
+})
 
 
+// Global error handling middleware
 
+app.use((err, req, res, next) => {
+    res.status(500).json("test");
+})
 
-app.listen(5000, () => {
-    console.log('app is running on port 5000');
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log('app is running on port ', PORT);
 })
