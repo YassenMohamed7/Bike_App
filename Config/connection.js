af0b6472ba0f8368f779ad50bdd8a7c175a14b84
YassenMohamed7 <yassenmohammed871@gmail.com>
2024-04-11 11:56:23 +0200

    update connections

--- Config/connection.js
+++ Config/connection.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./connection.json');
+const serviceAccount = require('./FirebaseConnection.json');
 
 initializeApp({
     credential: cert(serviceAccount)
