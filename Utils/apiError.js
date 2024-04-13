30932692972082d8b2421f1fdd1df8fd207a0ece
YassenMohamed7 <yassenmohammed871@gmail.com>
2024-04-13 15:35:33 +0200

    Adding global error handling

--- Utils/apiError.js
+++ Utils/apiError.js
@@ -1,11 +1,11 @@
 // @Desc this class is responsible for predictable errors
 
 class ApiError extends Error {
-    constructor(message, statusCode)
+    constructor(message, status)
     {
         super(message);
-        this.status= statusCode;
-        this.statusType = `${statusCode}`.startsWith('4')?"fail" : "error";
+        this.status= status;
+        this.statusType = `${status}`.startsWith('4')?"fail" : "error";
         this.isOperational = true
     }
 }
