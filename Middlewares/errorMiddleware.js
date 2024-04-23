d2a60e543c7cf859af9be7dec5e7747accb3130e
YassenMohamed7 <yassenmohammed871@gmail.com>
2024-04-23 14:37:35 +0200

    distinguish between development error and production error

--- Middlewares/errorMiddleware.js
+++ Middlewares/errorMiddleware.js
@@ -1,13 +1,30 @@
 
-const globalError = (err, req, res, next) => {
+
+const globalError = (err, req, res) => {
     err.status = err.status || 500;
     err.statusType = err.statusType || "error";
-    res.status(err.status).json({
+
+    if (process.env.ENVIRONMENT === 'development') {
+            sendToDevelopment(err, res);
+    }else{
+            sendToProduction(err, res);
+    }
+
+}
+
+
+const sendToDevelopment = (err, res)=>{
+    return  res.status(err.status).json({
         status: err.status,
         error: err,
         message: err.message,
         stack: err.stack
     });
 }
-
+const sendToProduction = (err, res)=>{
+    return  res.status(err.status).json({
+        status: err.status,
+        message: err.message,
+    });
+}
 module.exports = globalError;
