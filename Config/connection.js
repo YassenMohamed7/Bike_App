a00f8d9dd11abcf3ed2fa4814ffe0a9aaa1f7e0d
YassenMohamed7 <yassenmohammed871@gmail.com>
2024-04-24 09:27:29 +0200

    config storage bucket

--- Config/connection.js
+++ Config/connection.js
@@ -1,12 +1,17 @@
-const { initializeApp, cert } = require('firebase-admin/app');
+// const { initializeApp, cert , sto} = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-
+const admin = require('firebase-admin');
 const serviceAccount = require('./FirebaseConnection.json');
-
-initializeApp({
-    credential: cert(serviceAccount)
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "graduation-project-414621.appspot.com"
 });
-const db = getFirestore();
+
+// Get a reference to the Firebase Storage bucket
+const bucket = admin.storage().bucket();
+const db = admin.firestore();
+// const db = getFirestore();
 
 
-module.exports = db;
+
+module.exports = {db, bucket};
