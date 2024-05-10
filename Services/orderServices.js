a95c780e1dbd6f48554e47fb5bbb6d12bf575485
YassenMohamed7 <yassenmohammed871@gmail.com>
2024-05-10 16:09:06 +0300

    get all orders

--- Services/orderServices.js
+++ Services/orderServices.js
@@ -1,4 +1,3 @@
-const orders = require('../Models/orders');
 const asyncHandler = require('express-async-handler');
 const booking = require('../Models/booking');
 const vehicle = require("../Models/vehicleProfile");
@@ -51,24 +50,24 @@ exports.getOrders = asyncHandler(async (req, res, next) => {
 
 
 
-
-exports.getWeeklyOreders = asyncHandler(async (req, res) => {
-    const now = FirebaseFirestore.Timestamp.now();
-    const nowMillis = now.toMillis();
-    const sixDaysAgoMillis = nowMillis - (6 * 24 * 60 * 60 * 1000);
-    const sixDaysAgoTimestamp = new FirebaseFirestore.Timestamp(sixDaysAgoMillis);
-    const query = orders.where('date', ">=", sixDaysAgoTimestamp);
-    query.get().then((snapshot => {
-        if (snapshot.empty) {
-            // there is no orders this week.
-            return;
-        }
-        const users = new Set();
-        const totalRents = snapshot.docs.length;
-        snapshot.forEach((doc) => {
-            const orederId = doc.Customer_Id;
-            users.add(orederId);
-        });
-    }))
-
-})
+//
+// exports.getWeeklyOreders = asyncHandler(async (req, res) => {
+//     const now = FirebaseFirestore.Timestamp.now();
+//     const nowMillis = now.toMillis();
+//     const sixDaysAgoMillis = nowMillis - (6 * 24 * 60 * 60 * 1000);
+//     const sixDaysAgoTimestamp = new FirebaseFirestore.Timestamp(sixDaysAgoMillis);
+//     const query = orders.where('date', ">=", sixDaysAgoTimestamp);
+//     query.get().then((snapshot => {
+//         if (snapshot.empty) {
+//             // there is no orders this week.
+//             return;
+//         }
+//         const users = new Set();
+//         const totalRents = snapshot.docs.length;
+//         snapshot.forEach((doc) => {
+//             const orederId = doc.Customer_Id;
+//             users.add(orederId);
+//         });
+//     }))
+//
+// })
