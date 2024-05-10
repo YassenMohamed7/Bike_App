29c538ff0be4d67b07da91bc9034746f270359d7
YassenMohamed7 <yassenmohammed871@gmail.com>
2024-05-10 12:30:01 +0300

    refactor image uploading

--- Routes/categoryRoutes.js
+++ Routes/categoryRoutes.js
@@ -4,13 +4,14 @@ const {
     addCategory,
     getCategories} = require("../Services/categoryServices");
 
+const {uploadImage} = require('../Utils/uploadImage')
+
 
 const router = express.Router();
 
 
 router.route('/getCategories').get(getCategories);
 
-
-router.route('/addCategory').post(uploadCategoryImage, addCategory);
+router.route('/addCategory').post(uploadImage, addCategory);
 
 module.exports = router;
--- Services/categoryServices.js
+++ Services/categoryServices.js
@@ -1,34 +1,7 @@
 const categories = require('../Models/categories');
 const asyncHandler = require('express-async-handler');
-const multer = require('multer');
 const apiError = require('../Utils/apiError');
-const {bucket} = require("../Config/connection");
-
-// @desc configuring multer for uploading single image
-const multerStorage = multer.memoryStorage({
-    destination: function (req, file, cb) {
-        // no local storage, upload directly to Firebase bucket.
-      cb(null, '');
-    },
-    filename: function (req, file, cb) {
-        // mimetype is file_type/extension
-        const ext = file.mimetype.split('/')[1];
-        const filename = `${Date.now()}.${ext}`;
-        cb(null, filename);
-    },
-});
-const multerFilter = (req, file, cb) => {
-    const type = file.mimetype.split('/')[0];
-    if(type === 'image'){
-        cb(null, true);
-    }else{
-        cb(new apiError("Only images are allowed", 400), false);
-    }
-}
-const upload = multer({storage: multerStorage,
-    fileFilter: multerFilter});
-
-exports.uploadCategoryImage = upload.single('image');
+const uploadImage = require('../Utils/uploadImageToBucket');
 
 
 // add new category
@@ -38,30 +11,23 @@ exports.uploadCategoryImage = upload.single('image');
 exports.addCategory = asyncHandler(async (req, res, next) => {
     const provided_data = req.body;
     const file = req.file;
-    // generate image name
-    const ext = file.mimetype.split('/')[1];
-    const imageName = `category/${Date.now()}.${ext}`;
-    const imageFile = bucket.file(imageName);
-    const fileStream = imageFile.createWriteStream();
-    fileStream.on('error', () => {
-        next(new apiError("Error uploading image", 500));
-    });
+    const data= {
+        Category_Id: provided_data.id,
+        Category_Name: provided_data.name,
+        Description: provided_data.description,
+        Image: null
+    }
 
-    fileStream.on('finish', async ()=>{
-            const imageUrl = await imageFile.getSignedUrl({
-                action: 'read',
-                expires: '1-1-2030'
-            });
-        const data= {
-            Category_Id: provided_data.id,
-            Category_Name: provided_data.name,
-            Description: provided_data.description,
-            Image: imageUrl[0]
+
+    uploadImage(file, async (err, _imageUrl) => {
+        if (err) {
+            next(new apiError("Error Uploading Image", 500));
+        } else {
+            console.log("Image is: ", _imageUrl);
+            data.Image = _imageUrl;
+            await categories.doc(`${provided_data.id}`).create(data);
         }
-        await categories.doc(`${provided_data.id}`).create(data);
     });
-    fileStream.end(file.buffer);
-
 
     res.redirect('/api/v1/categories/getCategories');
 
